17d7260f351dd7e3f37679a4c1e682349cbf1a52
nikeedev <nikita.gon.dev@gmail.com>
2023-03-03 12:51:57 +0100

    updates

--- src/main.v
+++ src/main.v
@@ -1,15 +1,39 @@
 module main
 import os
 
+fn run(command string) {
+	mut code := os.execute(command)
+
+	if code.exit_code == 0 {
+		println("Done!")
+	}
+	else {
+		println(code.output)
+	}
+}
 
 fn main() {
 	if os.args.len < 2 {
 		println("giter: commmand-line helper for git - version 0.1.0-dev")
-		println("Usage: giter [command] --<args>")
+		println("Usage: giter [command] [arguments]")
 	}
 	else {
-		match(os.args[1]) {
-			
+		match os.args[1] {
+			"up" {
+				if os.args.len > 3 {
+					run("git commit -m '${os.args[2]}'")
+				}
+				else {
+					run("git commit -m 'updates'")
+				}
+			}
+			"addl" {
+				run("git add -A")
+			}
+			else {
+				println("giter: commmand-line helper for git - version 0.1.0-dev")
+				println("Usage: giter [command] [arguments]")
+			}
 		}
 	}
 }
