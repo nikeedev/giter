b1a86511a600b273e24b3fcf716dbc01124de0a1
nikeedev <nikita.gon.dev@gmail.com>
2023-12-22 16:48:57 +0100

    'updates'

--- src/main.v
+++ src/main.v
@@ -16,6 +16,7 @@ fn main() {
 	if os.args.len < 2 {
 		println("giter: commmand-line helper for git - version 0.1.0-dev")
 		println("Usage: giter [command] [arguments]")
+		println("\nCommands:\nup - does `git commit -m \"updates\"`, or custom message if passed after up command\nsave - commit using \"save\" comment\naddl - stage all files to git\ndall - nikee's setup, stage files, commit, pull, push\npush - push\npull - pull")
 	}
 	else {
 		match os.args[1] {
@@ -31,11 +32,23 @@ fn main() {
 					run("git commit -m 'updates'")
 				}
 			}
+			"dall" {
+				run("git add -A")
+				run("git commit -m 'updates'")
+				run("git pull")
+				run("git push")
+			}
 			"save" {
 				run("git commit -m 'save'")
 			}
 			"addl" {
 				run("git add -A")
+			}
+			"push" {
+				run("git push")
+			}
+			"fetch" {
+				run("git pull")
 			}
 			else {
 				println("giter: ${os.args[1]} not recognized as a command")
