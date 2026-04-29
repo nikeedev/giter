4888b93ced12fb100dd2afbeb045f6c2edb9582e
nikeedev <myftr12394@gmail.com>
2026-04-29 11:52:11 +0200

    'updating test'

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
     let args = env::args().collect::<Vec<String>>();
 
     if args.len() < 2 {
-        println!("giter: commmand-line helper for git - version 1.1.0");
+        println!("giter: commmand-line helper for git - version 1.2.0 - by nikeedev");
         println!("Usage: giter [command] [arguments]");
         println!(
             "\nCommands:\nup - does `git commit -m \"updates\"`, or custom message if passed after up command\nsave - commit using \"save\" comment\naddl - stage all files to git\ndall - nikee's setup, stage files, commit, pull, push\npush - push\npull - pull"
@@ -34,41 +34,42 @@ fn main() {
         "up" => {
             if args.len() > 2 {
                 let mut message = "".to_string();
-
-                for i in args {
+                
+                for i in args.iter().skip(2) {
                     message.push_str(i.as_str());
-                    message.push(' ');
                 }
-                run("git", format!("commit -m '{}'", message).as_str());
+
+                println!("git {:#?}", format!("commit -m \"{}\"", message).as_str());
+                run("git", format!("commit -m {}", message).as_str());
             } else {
-                run("git", "commit -m 'updates'");
+                run("git", "commit -m 'update'");
             }
         }
 
         "dall" => {
             run("git", "add -A");
-            run("git", "commit -m 'updates'");
+            run("git", "commit -m 'update'");
             run("git", "pull");
             run("git", "push");
         }
 
-        "save" => {
-            run("git", "commit -m 'save'");
-        }
         "addl" => {
             run("git", "add -A");
         }
+
         "push" => {
             run("git", "push");
         }
-        "fetch" => {
+        
+        "pull" => {
             run("git", "pull");
         }
+        
         _ => {
             println!("giter: {} not recognized as a command", args[1]);
             println!("Usage: giter [command] [arguments]");
             println!(
-                "\nCommands:\nup - does `git commit -m \"updates\"`, or custom message if passed after up command\nsave - commit using \"save\" comment\naddl - stage all files to git\ndall - nikee's setup, stage files, commit, pull, push\npush - push\npull - pull"
+                "\nCommands:\nup - does `git commit -m \"updates\"`, or custom message if passed after up command\naddl - stage all files to git\ndall - nikee's setup, stage files, commit, pull, push\npush - push\npull - pull"
             );
             exit(1);
         }
